365e264a36cfd7ca9aaf9fcfaee993a3cce0528d
Fredrik Ekholdt <fredrik.ekholdt@typesafe.com>
2013-05-14 16:31:53 +0200

    zip down stuff

--- files/adept-ivy-cache.sh
+++ files/adept-ivy-cache.sh
@@ -3,12 +3,15 @@
 http_prefix=http://freekh.github.io/adept/files/
 jar_location="$http_prefix"adept-tools-assembly-0.1-SNAPSHOT.jar
 jar_file=$(mktemp -t adept-ivy)
+zip_file=$(mktemp -t adept-ivy)
 output=$(mktemp -t adept-ivy)
 
-echo "downloading adept ivy scanner..."
+echo "downloading adept ivy scanner (includes scala libraries so be patient!) to: $jar_file..."
 curl $jar_location > $jar_file
 
 
 java -jar $jar_file $output
 
-echo "send file: $output to freekh@gmail.com"
+zip "$zip_file.zip" $output
+
+echo "send file: $zip_file to fredrik.ekholdt@typesafe.com"
