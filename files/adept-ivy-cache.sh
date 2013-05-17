4e2849391139913d7444c68c3dc23473b5f22cb1
Fredrik Ekholdt <fredrik.ekholdt@typesafe.com>
2013-05-17 12:07:22 +0200

    fixed wrong location download

--- files/adept-ivy-cache.sh
+++ files/adept-ivy-cache.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-http_prefix=http://freekh.github.io/adept/files/
-jar_location="$http_prefix$jar_name"
 tmp_dir=$PWD/tmp
 mkdir -p $tmp_dir
+
+
+http_prefix=http://freekh.github.io/adept/files/
+
+scala_version="2.9.2"
+
+adept_tools_version="0.1-SNAPSHOT"
+
 report_name="report-$(date +%s)"
+
 output="$tmp_dir/$report_name"
 zip_file="$PWD/$report_name.zip"
-scala_version="2.9.2"
-adept_tools_version="0.1-SNAPSHOT"
+
 jar_name="adept-tools_$scala_version-$adept_tools_version.jar"
+jar_location="$http_prefix$jar_name"
 jar_file="$tmp_dir/$jar_name"
 scala_lib="$HOME/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-$scala_version.jar"
 main_class="adept.tools.AdeptIvyCacheReader"
@@ -23,7 +30,7 @@ if [ ! -f $scala_lib  ]; then
 fi
 
 
-echo "downloading adept ivy scanner to: $jar_file..."
+echo "downloading adept ivy scanner from $jar_location to $jar_file..."
 curl $jar_location > $jar_file
 
 cmd="java -cp $scala_lib:$jar_file $main_class $output"
@@ -33,13 +40,11 @@ eval $cmd
 echo "zipping to $zip_file..."
 zip "$zip_file" $output
 
-
-
 read -p "hit any key to clean up (remove $tmp_dir), or do ctrl+c to abort..."
-echo "cleaning up..."
 
 rm -r $tmp_dir
 
+echo "removed $tmp_dir!"
 
 echo "send file: $zip_file.zip to fredrik.ekholdt@typesafe.com"
 
